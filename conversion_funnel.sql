a3c7fe68bf814dbcf75afc7429c472a8e3b57b36
shivams334 <108360839+shivams334@users.noreply.github.com>
2022-08-05 02:24:13 +0530

    Update conversion_funnel.sql

--- conversion_funnel.sql
+++ conversion_funnel.sql
@@ -1,16 +1,16 @@
 select 
 	website_session_id,
 	max(products_pages) as product_made_it,
-    max(mr_fuzzy_pages) as mr_fuzzy_made_it,
-    max(cart_pages) as cart_made_it
+    	max(mr_fuzzy_pages) as mr_fuzzy_made_it,
+    	max(cart_pages) as cart_made_it
 from (
 select 
 	website_sessions.website_session_id, 
-    website_pageviews.pageview_url, 
-    website_pageviews.created_at as pageview_created_at,
-    case when website_pageviews.pageview_url = '/products' then 1 else 0 end as products_pages,
-    case when website_pageviews.pageview_url = '/the-original-mr-fuzzy' then 1 else 0 end as mr_fuzzy_pages,
-    case when website_pageviews.pageview_url = '/cart' then 1 else 0 end as cart_pages
+    	website_pageviews.pageview_url, 
+    	website_pageviews.created_at as pageview_created_at,
+    	case when website_pageviews.pageview_url = '/products' then 1 else 0 end as products_pages,
+    	case when website_pageviews.pageview_url = '/the-original-mr-fuzzy' then 1 else 0 end as mr_fuzzy_pages,
+    	case when website_pageviews.pageview_url = '/cart' then 1 else 0 end as cart_pages
 from website_sessions
 left join website_pageviews
 on website_sessions.website_session_id = website_pageviews.website_session_id
